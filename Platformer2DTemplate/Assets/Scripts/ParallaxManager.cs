de9eb9d4cd2625e0a3a5c6d8cc85a8357f966702
Remi <remi.fusade@gmail.com>
2015-12-19 14:25:18 +0100

    Add Font Fix UI Resizing Introduce decoration stuff (with parallax)
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ Platformer2DTemplate/Assets/Scripts/ParallaxManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParallaxManager : MonoBehaviour {
+
+	public Camera currentCamera;
+
+	public List<GameObject> decorationLayers;
+
+	Vector3 lastCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 cameraMovement = currentCamera.transform.position - lastCameraPosition;
+
+		foreach (GameObject decorationLayer in decorationLayers)
+		{
+			float zDelta = decorationLayer.transform.position.z;
+		}
+	}
+
+	void LateUpdate()
+	{
+		lastCameraPosition = currentCamera.transform.position;
+	}
+}
